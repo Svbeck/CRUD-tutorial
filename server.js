7d2648687d06f52b9455c1d72d87db2e4d8bd110
Svbeck <stianvb@msn.com>
2018-04-15 14:43:43 +0200

    Update part of tutorial

--- server.js
+++ server.js
@@ -5,8 +5,12 @@ var MongoClient = require('mongodb').MongoClient
 var url = 'mongodb://Svbeck:Gullf1sk3n@ds157521.mlab.com:57521/blackscreen'
 var db
 
+
+
+
 app.use(bodyParser.urlencoded({extended: true}))
-app.use(express.static('public'))
+app.use('/public', express.static(__dirname+'/public'))
+app.use(bodyParser.json())
 //app.set('view-engine','ejs')
 app.set('views', __dirname + '/views');
 
@@ -32,4 +36,18 @@ app.get('/', function(req,res){
 		res.render('index.ejs', {quotes: result} )					//write data to index.ejs
 	})
 })
+
+app.put('/quotes', function(req,res){
+	db.collections('quotes').findOneAndUpdate({name: 'Yoda'},	//query
+		{$set: 	{name: req.body.name,							//update
+				quote: req.body.quote}
+				},
+				{ 	sort: {__id:-1}, 
+					upsert: true
+				}, (err, result) => {
+				if (err) return res.send(err)
+					console.log(req.body)
+					res.send(result)})
+})
+
 
